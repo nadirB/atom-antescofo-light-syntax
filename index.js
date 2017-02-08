0202c142fe995ec8a6f679e9016c3f1eca65b0d0
nadirb <hecceite@free.fr>
2017-02-08 14:17:42 +0100

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   activate() {
-    require('atom-package-deps').install('atom-antescofo-syntax')
+    require('atom-package-deps').install('atom-antescofo-light-syntax')
       .then(function() {
         console.log('All dependencies installed, good to go')
       })
